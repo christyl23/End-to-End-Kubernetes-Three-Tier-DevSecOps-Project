23fed581b961481185b8f4faea84f8f4f708603d
christyl23 <christyl23@gmail.com>
2024-04-27 21:29:33 -0400

    updates

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mynew-bucket-24"
+    bucket         = "advanced-end-to-end-devsecops-new"
     region         = "us-west-2"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "products"
