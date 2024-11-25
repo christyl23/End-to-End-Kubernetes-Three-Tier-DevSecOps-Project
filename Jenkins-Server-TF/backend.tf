d9ce7a4658e6d762395302279d7b5f5071a22505
christyl23 <120269677+christyl23@users.noreply.github.com>
2024-11-24 20:15:29 -0500

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terra-state-dove24"
-    region         = "us-west-2"
+    bucket         = "terracli"
+    region         = "us-east-2"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "orders"
+    dynamodb_table = "e-t-ek8"
     encrypt        = true
   }
   required_version = ">=0.13.0"
